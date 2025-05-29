843fbf0317cf7268f28782b0b02d24d83bfb95f7
shinobi9 <shinobi9c@outlook.com>
2025-05-29 21:17:59 +0800

    根据当前活跃的文件判断工作区生成文件，使用系统的EOL

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
-const vscode = require("vscode");
 const path = require("path");
 const fs = require("fs");
+const os = require("os");
+
+const EOL = os.EOL;
 
 let disposableCommand;
 let disposableCompletionItemProvider;
@@ -29,30 +31,41 @@ function regitserCommands(context) {
     include: ["**/*.js"],
   };
 
-  disposableCommand = vscode.commands.registerCommand(
-    "beidoums-scripts-snippets.create-jsconfig",
-    async () => {
-      const workspaceFolders = vscode.workspace.workspaceFolders;
-      if (!workspaceFolders) {
-        vscode.window.showErrorMessage("open workspace first!");
+  disposableCommand = vscode.commands.registerCommand("beidoums-scripts-snippets.create-jsconfig", async () => {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {
+      vscode.window.showErrorMessage("open workspace first!");
+      return;
+    }
+
+    const activeEditor = vscode.window.activeTextEditor;
+    if (!activeEditor) {
+      vscode.window.showErrorMessage("open workspace first!");
+      return;
+    }
+    const activeEditorUri = activeEditor.document.uri;
+
+    const activeWorkspace = vscode.workspace.getWorkspaceFolder(activeEditorUri);
+    if (!activeWorkspace) {
+      vscode.window.showErrorMessage("open workspace first!");
+      return;
+    }
+
+    const rootPath = activeWorkspace.uri.fsPath;
+
+    const jsconfigPath = path.join(rootPath, "jsconfig.json");
+
+    try {
+      if (fs.existsSync(jsconfigPath)) {
+        vscode.window.showErrorMessage("jsconfig.json exists, won't generate");
         return;
       }
-
-      const rootPath = workspaceFolders[0].uri.fsPath;
-      const jsconfigPath = path.join(rootPath, "jsconfig.json");
-
-      try {
-        if (fs.existsSync(jsconfigPath)) {
-          vscode.window.showErrorMessage("jsconfig.json exists, won't generate");
-          return
-        }
-        fs.writeFileSync(jsconfigPath, JSON.stringify(jsconfigContent, null, 2), "utf8");
-        vscode.window.showInformationMessage("jsconfig.json create success!");
-      } catch (error) {
-        vscode.window.showErrorMessage(`error in creating jsconfig.json: ${error}`);
-      }
+      fs.writeFileSync(jsconfigPath, JSON.stringify(jsconfigContent, null, 2), "utf8");
+      vscode.window.showInformationMessage("jsconfig.json create success!");
+    } catch (error) {
+      vscode.window.showErrorMessage(`error in creating jsconfig.json: ${error}`);
     }
-  );
+  });
   context.subscriptions.push(disposableCommand);
 }
 
@@ -61,41 +74,33 @@ function registerCompletionItems(context) {
   const dtsPath = getDtsPath(context);
   const triggerText = "/";
   const prefix = "///";
-  const text = `/// <reference no-default-lib="true"/>\n/// <reference path="${dtsPath}" />\n\n\$0`;
+
+  const text = `/// <reference no-default-lib="true"/>${EOL}/// <reference path="${dtsPath}" />${EOL}${EOL}\$0`;
   const detail = "指定类型定义文件";
   const documentation = "指定类型定义文件";
-  disposableCompletionItemProvider =
-    vscode.languages.registerCompletionItemProvider(
-      "javascript",
-      {
-        provideCompletionItems(document, position) {
-          const linePrefix = document
-            .lineAt(position)
-            .text.substring(0, position.character);
+  disposableCompletionItemProvider = vscode.languages.registerCompletionItemProvider(
+    "javascript",
+    {
+      provideCompletionItems(document, position) {
+        const linePrefix = document.lineAt(position).text.substring(0, position.character);
 
-          if (!linePrefix.endsWith(triggerText)) {
-            return undefined;
-          }
+        if (!linePrefix.endsWith(triggerText)) {
+          return undefined;
+        }
 
-          const startPos = new vscode.Position(
-            position.line,
-            linePrefix.lastIndexOf(triggerText)
-          );
+        const startPos = new vscode.Position(position.line, linePrefix.lastIndexOf(triggerText));
 
-          const completionItem = new vscode.CompletionItem(
-            prefix,
-            vscode.CompletionItemKind.Snippet
-          );
-          completionItem.range = new vscode.Range(startPos, position);
-          completionItem.insertText = new vscode.SnippetString(text);
-          completionItem.detail = detail;
-          completionItem.documentation = documentation;
-          completionItem.filterText = prefix;
-          return [completionItem];
-        },
+        const completionItem = new vscode.CompletionItem(prefix, vscode.CompletionItemKind.Snippet);
+        completionItem.range = new vscode.Range(startPos, position);
+        completionItem.insertText = new vscode.SnippetString(text);
+        completionItem.detail = detail;
+        completionItem.documentation = documentation;
+        completionItem.filterText = prefix;
+        return [completionItem];
       },
-      triggerText
-    );
+    },
+    triggerText
+  );
   context.subscriptions.push(disposableCompletionItemProvider);
 }
 
